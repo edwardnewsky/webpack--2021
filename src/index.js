d49b934c619771605901e181b5cd70cf36950545
EdwardNewsky <edwardnewsky@gmail.com>
2021-05-04 13:19:55 +0300

    лоадер XML и CSV

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Post from './Post';
 import json from './assets/json.json';
+import xml from './assets/data.xml';
 import WebpackLogo from './assets/webpack-logo.png';
 import './styles/styles.css';
 
@@ -7,4 +8,5 @@ const post = new Post('Webpack Post Title', WebpackLogo);
 
 console.log('Post to String', post.toString());
 
-console.log('JSON', json);
+console.log('JSON:', json);
+console.log('XML:', xml);
